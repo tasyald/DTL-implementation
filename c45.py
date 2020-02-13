af67dff482dc73a471446a9fab9c78963efc6362
Juro <jsutantra@gmail.com>
2020-02-13 18:48:02 +0700

    handle continuouos attributes

--- c45.py
+++ c45.py
@@ -36,7 +36,7 @@ def information_gain (dictionary) :
     # print(total_entropy_dict)
     # print(total_dataset)
     entropy_total = entropy_count(total_entropy_dict["total"])
-    print(entropy_total)
+    # print(entropy_total)
     temp_gain = 0
     for attr in dictionary :        
         count_total_attribute = 0
@@ -44,28 +44,63 @@ def information_gain (dictionary) :
         for target in dictionary[attr] : 
             count_total_attribute = count_total_attribute + dictionary[attr][target] 
         temp_gain = temp_gain - (count_total_attribute/total_dataset) * entropy_attr
-    print("INI GAINNYA")
-    print(entropy_total + temp_gain)
-    
+    # print("INI GAINNYA")
+    # print(entropy_total + temp_gain)
+    return entropy_total + temp_gain
         # entropy_count(attrdict[attr])
 
 def continouous_value (data, target) :
     tempdict = {}
     for cell in target : 
-        tempdict[cell[0]] = 0
+        tempdict[str(cell[0])] = 0
     # initdict = copy.deepcopy(tempdict)
-    print(tempdict)
-    for row in data:
-        attrdict = {}
-        # tempdict = copy.deepcopy(initdict)
-        for i in range(0,len(row)): 
-            if row[i] in attrdict:
-                attrdict[row[i]][target[i][0]] = attrdict[row[i]][target[i][0]] + 1 
-            else :
-                attrdict[row[i]] = copy.deepcopy(tempdict)
-                attrdict[row[i]][target[i][0]] = attrdict[row[i]][target[i][0]] + 1 
-        print(attrdict)
-        information_gain(attrdict)
+    # print(tempdict)
+    # print(data)
+    init_target = copy.deepcopy(target)
+    # sortattr = row.
+    attrdict = {}
+    for i in range(0,len(data)): 
+        target[i].append(data[i])
+    target.sort(key=itemgetter(0), reverse=False)
+    target.sort(key=itemgetter(1), reverse=False)
+    # print(target)
+    # tempdict = copy.deepcopy(initdict)
+    previous_value = target[0][0]
+    # for i in range(0,len(target)):
+    #     if target[i][1] in attrdict :
+    #         attrdict[target[i][1]][target[i][0]] = attrdict[target[i][1]][target[i][0]] + 1 
+    #     else :
+    #         attrdict[target[i][1]] = copy.deepcopy(tempdict)
+    #         attrdict[target[i][1]][target[i][0]] = attrdict[target[i][1]][target[i][0]] + 1 
+    # best_gain = information_gain(attrdict)
+    best_gain = -999
+    for j in range(1,len(target)) :
+        if not(previous_value == target[j][0]):
+            # print(previous_value)
+            previous_value = target[j][0]
+            attrdict = {"<"+str(target[j+1][1]):copy.deepcopy(tempdict),">="+str(target[j+1][1]):copy.deepcopy(tempdict)}
+            for i in range(0,len(target)):
+                if target[i][1]<target[j+1][1] :
+                    attrdict["<"+str(target[j+1][1])][str(target[i][0])] = attrdict["<"+str(target[j+1][1])][str(target[i][0])] + 1 
+                else:
+                    attrdict[">="+str(target[j+1][1])][str(target[i][0])] = attrdict[">="+str(target[j+1][1])][str(target[i][0])] + 1 
+                # if target[i][1]<previous_value :
+                #     attrdict[target[i][1]][target[i][0]] = attrdict[target[i][1]][target[i][0]] + 1 
+                # else :
+                #     attrdict[target[i][1]] = copy.deepcopy(tempdict)
+                #     attrdict[target[i][1]][target[i][0]] = attrdict[target[i][1]][target[i][0]] + 1 
+            # print(attrdict)
+            if best_gain < information_gain(attrdict) :
+                best_gain = information_gain(attrdict)
+                potential_split = (target[j][1]+target[j-1][1])/2
+        else :
+            pass
+    # print("======================")
+    # print(potential_split)
+    # print(best_gain)
+    return(potential_split,best_gain)
+    target = copy.deepcopy(init_target)
+        
         
         
 
@@ -87,8 +122,13 @@ def missing_value(data, target):
 
 # Read iris dataset
 data_iris = datasets.load_iris()
-data_iris
-data_iris['target']
+# print(data_iris)
+data_iris_target = data_iris['target'].tolist()
+data_iris_data = data_iris['data'].tolist()
+print(data_iris_data)
+for i in range(0,len(data_iris_target)) :
+    data_iris_target[i] = [data_iris_target[i]]
+print(data_iris_target)
 
 # Read play-tennis dataset
 data_play_tennis = pd.read_csv('play-tennis.csv')
@@ -108,6 +148,9 @@ for x in data_play_tennis:
 data_play_tennis_data
 
 missing_value(data_play_tennis_data, data_play_tennis_target)
-data_play_tennis_data = list(zip(*data_play_tennis_data))
+from operator import itemgetter
+
 print(data_play_tennis_data[0])
-continouous_value(data_play_tennis_data, data_play_tennis_target)
+data_iris_data = list(zip(*data_iris_data))
+data_play_tennis_data = list(zip(*data_play_tennis_data))
+print(continouous_value(data_play_tennis_data[0], data_play_tennis_target))
