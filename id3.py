51a9d45ff64b7939cc8ae0d1ba4c51730d7c6a0c
Kevin Angelo <kevinn.angelo@gmail.com>
2020-02-13 18:39:13 +0700

    finished id3

--- id3.py
+++ id3.py
@@ -17,7 +17,8 @@ def attribute_table(data, target):
                 attrins[row[i]] = copy.deepcopy(attrtarget) 
             attrins[row[i]][target[i][0]] += 1
         attrdict[row] = attrins
-    print(attrdict)
+
+    return attrdict
 
 def entropy_count (dictionary) :
     sumentropy = 0
@@ -29,8 +30,8 @@ def entropy_count (dictionary) :
         probability = dictionary[item]/counttotal
         if not (probability ==0):
             sumentropy = sumentropy - (probability)*math.log2(probability)
-    print(sumentropy)
-    pass
+    
+    return sumentropy
 
 def information_gain (dictionary) :
     
@@ -58,10 +59,8 @@ def information_gain (dictionary) :
         for target in dictionary[attr] : 
             count_total_attribute = count_total_attribute + dictionary[attr][target] 
         temp_gain = temp_gain - (count_total_attribute/total_dataset) * entropy_attr
-    print("INI GAINNYA")
-    print(entropy_total + temp_gain)
-    
-        # entropy_count(attrdict[attr])
+
+    return entropy_total + temp_gain
 
 def remove_root(data, target, val):
     dataret, targetret = [], []
@@ -75,6 +74,30 @@ def remove_root(data, target, val):
 
     return dataret, targetret
 
+def id3(data, target, tree=None):
+    if tree is None:
+        tree = {}
+
+    if entropy_count(entropy_table(data, target)) == 0:
+        tree = {target[0]}
+    else:
+        gainParam = attribute_table(data, target)
+        gainList = []
+        for i in gainParam:
+            res = information_gain(i)
+            gainList.append(res)
+        root = gainList.index(max(gainList))
+        rootVal = attName[root]
+
+        for i in list_value(root, data):
+            dataret, targetret = remove_root(data, target, i)
+            if entropy_count(entropy_table(data, target)) == 0:
+                tree[rootVal][i] = targetret[0]
+            else:
+                tree[rootVal][i] = id3(dataret, targetret)
+
+    return tree
+
 # Read iris dataset
 data_iris = datasets.load_iris()
 data_iris
@@ -82,6 +105,7 @@ data_iris['target']
 
 # Read play-tennis dataset
 data_play_tennis = pd.read_csv('play-tennis.csv')
+attName = data_play_tennis.columns.tolist()
 data_play_tennis = data_play_tennis.values.tolist()
 data_play_tennis
 
@@ -96,11 +120,6 @@ data_play_tennis_data = []
 for x in data_play_tennis:
     data_play_tennis_data.append(x[:-1][1:])
 data_play_tennis_data
-
-# remote root
-data, target = remove_root(data_play_tennis_data, data_play_tennis_target, 'Sunny')
-for i in data:
-    print(i)
 
 data_play_tennis_data = list(zip(*data_play_tennis_data))
 # print(data_play_tennis_data[0])
