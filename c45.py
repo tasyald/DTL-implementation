9a9907e4f80b34e3253cf8cd9e58844ec203fabd
tasyald <13517141@std.stei.itb.ac.id>
2020-02-13 14:27:30 +0700

    Added split info

--- c45.py
+++ c45.py
@@ -11,23 +11,14 @@ def entropy_count (dictionary) :
     counttotal = 0
     for item in dictionary :
         counttotal = counttotal + dictionary[item]
-    # print(counttotal)
     for item in dictionary :
         probability = dictionary[item]/counttotal
         if not (probability ==0):
             sumentropy = sumentropy - (probability)*math.log2(probability)
-    print(sumentropy)
-    pass
-	
-    # print(sumentropy)
     return sumentropy
 
 def information_gain (dictionary) :
-    
-    # print(dictionary)
-    # print("jancok")
     total_entropy_dict = {"total" : {}}
-    
     for attr in dictionary :
         for target in dictionary[attr] : 
             if target in total_entropy_dict["total"] :
@@ -36,9 +27,7 @@ def information_gain (dictionary) :
                 total_entropy_dict["total"][target] = dictionary[attr][target]; 
     total_dataset = 0
     for target in total_entropy_dict["total"]:
-        total_dataset = total_dataset + total_entropy_dict["total"][target] 
-    # print(total_entropy_dict)
-    # print(total_dataset)
+        total_dataset = total_dataset + total_entropy_dict["total"][target]
     entropy_total = entropy_count(total_entropy_dict["total"])
     print(entropy_total)
     temp_gain = 0
@@ -50,15 +39,36 @@ def information_gain (dictionary) :
         temp_gain = temp_gain - (count_total_attribute/total_dataset) * entropy_attr
     print("INI GAINNYA")
     print(entropy_total + temp_gain)
+
+def split_information(dictionary):
+    print(dictionary)
+    total_entropy_dict = {"total" : {}}
+    for attr in dictionary:
+        for target in dictionary[attr] : 
+            if target in total_entropy_dict["total"] :
+                total_entropy_dict["total"][target] = total_entropy_dict["total"][target]+ dictionary[attr][target]; 
+            else :
+                total_entropy_dict["total"][target] = dictionary[attr][target]; 
+    total_dataset = 0
+    for target in total_entropy_dict["total"]:
+        total_dataset = total_dataset + total_entropy_dict["total"][target]
+    split = 0
+    for attr in dictionary :        
+        count_total_attribute = 0
+        entropy_attr = entropy_count(dictionary[attr])
+        for target in dictionary[attr] : 
+            count_total_attribute = count_total_attribute + dictionary[attr][target] 
+        split = split - (count_total_attribute/total_dataset) * math.log2(count_total_attribute/total_dataset)
+    print("INI SPLIT INFORMATION")
+    print(split)
     
-        # entropy_count(attrdict[attr])
 
 def continouous_value (data, target) :
     tempdict = {}
     for cell in target : 
         tempdict[cell[0]] = 0
     # initdict = copy.deepcopy(tempdict)
-    print(tempdict)
+    # print(tempdict)
     for row in data:
         attrdict = {}
         # tempdict = copy.deepcopy(initdict)
@@ -68,8 +78,9 @@ def continouous_value (data, target) :
             else :
                 attrdict[row[i]] = copy.deepcopy(tempdict)
                 attrdict[row[i]][target[i][0]] = attrdict[row[i]][target[i][0]] + 1 
-        print(attrdict)
+        # print(attrdict)
         information_gain(attrdict)
+        # split_information(attrdict)
         
         
 def missing_value(data, target):
@@ -109,5 +120,4 @@ data_play_tennis_data
 
 missing_value(data_play_tennis_data, data_play_tennis_target)
 data_play_tennis_data = list(zip(*data_play_tennis_data))
-print(data_play_tennis_data[0])
 continouous_value(data_play_tennis_data, data_play_tennis_target)
